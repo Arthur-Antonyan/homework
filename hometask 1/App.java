5d8c1fd26854668261505ab83847abd04dacb09a
Arthur-Antonyan <92539831+Arthur-Antonyan@users.noreply.github.com>
2021-10-17 01:45:44 +0400

    Add files via upload

---
+++ hometask 1/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) {
+        byte a = 23;
+        short b = 155;
+        int c = 260;
+        float d = 5.26F;
+        long e = 1012L;
+        double f = 546.37;
+        char g = '\u00EA';
+        boolean isRight = true;
+        String i = "hello world";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(isRight);
+        System.out.println(i);
+
+            
+                
+            
+            
+    
+    }
+}
