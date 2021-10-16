c1bb43b29ba0ac4c97dbe50194741cdb980cc30e
Arthur-Antonyan <92539831+Arthur-Antonyan@users.noreply.github.com>
2021-10-17 01:32:40 +0400

    Add files via upload

---
+++ hometask 1/App.java
@@ -0,0 +1,77 @@
+public class App {
+    public static void main(String[] args) {
+        // byte a = 23;
+        // short b = 155;
+        // int c = 260;
+        // float d = 5.26F;
+        // long e = 1012L;
+        // double f = 546.37;
+        // char g = '\u00EA';
+        // boolean isRight = true;
+        // String i = "hello world";
+
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(d);
+        // System.out.println(e);
+        // System.out.println(f);
+        // System.out.println(g);
+        // System.out.println(isRight);
+        // System.out.println(i);
+
+            // HOMETASK 6
+         
+            // int n = 100;
+            // for(int i = 0; i<n; i++){
+            //     if((i % 3 ==0) && (i%5 == 0)){
+            //         System.out.println(i);
+
+            //     }
+                
+            // } 
+
+               
+        // HOMETASK 7
+
+        //     int n = 1284;
+        //     int a = Math.round((n/100)%10);
+        //     int b = Math.round(n/1000);
+        //     int c = Math.round((n/10)%10);
+        //     int d = Math.round(n%10);
+        //     int result = a + b + c + d;
+        //     if(result % 2 != 0 ){
+        //     System.out.println("Oddish");
+
+        //     }else{ System.out.println("Evenish");
+        // };
+
+
+                // HOMETASK 8
+
+
+
+            //     int n = 100;
+            // for(int i = n; i!=0; i--){
+              
+            //         System.out.println(i);
+
+            //     }
+
+
+            //HOMETASK 9
+
+            // int n = 10;
+            // for(int i = 1; i<=n; i++){
+                
+            //         System.out.println("7x" + i+ "=" + 7*i);
+
+                
+                
+            // } 
+                
+            
+            
+    
+    }
+}
