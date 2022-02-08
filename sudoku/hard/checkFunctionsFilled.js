7900eab20610cfec863b61e95d8ed2bb6dd565ad
Arthur-Antonyan <92539831+Arthur-Antonyan@users.noreply.github.com>
2022-02-08 21:33:35 +0400

    Add files via upload

---
+++ sudoku/hard/checkFunctionsFilled.js
@@ -0,0 +1,31 @@
+export let answersFilled = [];
+export const checkRowsArrFilled = [];
+export const checkGridArrFilled = [];
+export const checkColumnsArrFilled = [];
+export function checkGridFilled(matrix) {
+  answersFilled.length = 0;
+  checkRowsArrFilled.length = 0;
+  checkColumnsArrFilled.length = 0;
+  let z, z1, z2;
+  for (let i = 0; i < matrix.length; i++) {
+    checkRowsArrFilled.splice(0, z);
+    checkColumnsArrFilled.splice(0, z2);
+    // checkGridArrFilled.splice(0, z1);
+    for (let j = 0; j < matrix[i].length; j++) {
+      checkRowsArrFilled.push(matrix[i][j]);
+      // checkGridArrFilled.push(matrix[i][j]);
+      checkColumnsArrFilled.push(matrix[j][i]);
+      z = checkRowsArrFilled.length;
+      // z1 = checkGridArrFilled.length;
+      z2 = checkColumnsArrFilled.length;
+    }
+
+    const x = new Set(checkRowsArrFilled);
+    // const y = new Set(checkGridArrFilled);
+    const g = new Set(checkColumnsArrFilled);
+
+    if (x.size != z || g.size != z2) {
+      answersFilled.push(false);
+    }
+  }
+}
